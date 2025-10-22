6d150da849810d96a939cb90ed5ce746cd809fbc
sfiorenzaaa <sfiorenza@student.ciputra.ac.id>
2025-10-22 09:28:34 +0700

    Lastpage

---
+++ app/src/main/java/com/example/testkelas/Lastpage.kt
@@ -0,0 +1,24 @@
+package com.example.testkelas
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.testkelas.ui.theme.TestkelasTheme
+
+@Composable
+fun Lastpage(){
+    Column {
+        Text(
+            text="Halo"
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun LastpagePreview() {
+    TestkelasTheme {
+        Lastpage()
+    }
+}
